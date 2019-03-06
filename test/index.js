ff33f521f2334d8e3ed94d742fa9352fd4241f80
NellyNi <35545690+NellyNi@users.noreply.github.com>
2019-03-05 18:09:55 -0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,3 +1,7 @@
+import React from 'react';
+import { shallow } from "enzyme";
+import HomeScreen from '../app/screens/HomeScreen';
+
 var assert = require('assert');
 describe('HelloWorld Module', function() {
   it('should return -1 when "Hello" is missing', function() {
@@ -6,4 +10,14 @@ describe('HelloWorld Module', function() {
   it('should return 0 when sentence starts with Hello', function() {
     assert.equal(0, "Hello World, how are you?".indexOf("Hello"));
   });
+});
+
+describe('Testing setting hours', function() {
+	it('Should set the sleeping hours to 8', ()=> {
+    var wake = '01 Jan 2018 07:00:00 GMT';
+    const time = shallow(<HomeScreen ti= wake/>);
+    var sleep = '11:00 PM';
+    
+		assert.equal(sleep,time.sleepTime);
+	});
 });
