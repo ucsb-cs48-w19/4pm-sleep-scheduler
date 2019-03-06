0750c372a3a689c90a1d43a0712d6f6f098622c6
hseong42 <hseong42@users.noreply.github.com>
2019-03-05 22:12:48 -0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -8,3 +8,13 @@ describe('HelloWorld Module', function() {
     assert.equal(0, "Hello World, how are you?".indexOf("Hello"));
   });
 });
+
+describe('Testing sleep calculator', function() {
+	it('should return 9:00 - 8 hours = 1:00', function(){
+		const sleepFunction = require("../sleepCalc.js");
+		var time = 9;
+		var hours = 8;
+		var result = sleepFunction.sleepTime(9,1);
+		assert.equal("1:00",result);
+	});
+});
