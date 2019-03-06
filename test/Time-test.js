f1d4f3720f0b167bd2f98efdae5a1e1cf463ceb3
NellyNi <35545690+NellyNi@users.noreply.github.com>
2019-03-05 18:16:30 -0800

    Update Time-test.js

--- test/Time-test.js
+++ test/Time-test.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from "react-native";
 import { shallow } from "enzyme";
 import HomeScreen from '../app/screens/HomeScreen';
 
 describe("<HomeScreen/>", () => {
-  it('Should set the sleeping hours to 8', ()=> {
+  it("Should set the sleeping hours to 8", ()=> {
      var wake = '01 Jan 2018 07:00:00 GMT';
      const time = shallow(<HomeScreen ti= wake/>);
      var sleep = '11:00 PM';
