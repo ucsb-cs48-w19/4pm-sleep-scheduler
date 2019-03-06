c4dfb26436fa7805b041c8a114451a8421da5701
hseong42 <hseong42@users.noreply.github.com>
2019-03-05 22:42:19 -0800

    Update sleepCalc.js

--- sleepCalc.js
+++ sleepCalc.js
@@ -1,5 +1,5 @@
+import moment from 'moment';
 module.exports = {
-	import moment from "moment";
 
 	 sleepTime: function(time1, time2) {
 		var sleep = moment(time1).subtract(time2, "hours").format("LT");
