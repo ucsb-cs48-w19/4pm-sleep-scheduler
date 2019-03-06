a1c75cbbf988c9b8d3f955b971011c94d4aa36fc
NellyNi <35545690+NellyNi@users.noreply.github.com>
2019-03-05 18:15:10 -0800

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,7 +1,3 @@
-import React from 'react';
-import { shallow } from "enzyme";
-import HomeScreen from '../app/screens/HomeScreen';
-
 var assert = require('assert');
 describe('HelloWorld Module', function() {
   it('should return -1 when "Hello" is missing', function() {
@@ -12,12 +8,3 @@ describe('HelloWorld Module', function() {
   });
 });
 
-describe("<HomeScreen/>", () => {
-  it('Should set the sleeping hours to 8', ()=> {
-     var wake = '01 Jan 2018 07:00:00 GMT';
-     const time = shallow(<HomeScreen ti= wake/>);
-     var sleep = '11:00 PM';
-    
-     assert.equal(sleep,time.sleepTime);
-  });
-});
