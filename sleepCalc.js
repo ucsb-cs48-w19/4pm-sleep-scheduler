4310a0cf88d613e0665691b6ab940ba63ed14584
hseong42 <hseong42@users.noreply.github.com>
2019-03-05 22:36:57 -0800

    Update sleepCalc.js

--- sleepCalc.js
+++ sleepCalc.js
@@ -1,11 +1,8 @@
 module.exports = {
 
 	 sleepTime: function(time1, time2) {
-     var sleep = time1 - time2;		
-      if(sleep <= 0) {
-        sleep = sleep + 24;
-      } 
-      return sleep + ":00";
+		var sleep = moment(time1).subtract(time2, "hours").format("LT");
+     		return sleep;
 	},
 	
 };
